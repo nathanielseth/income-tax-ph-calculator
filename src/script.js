df09d97008e331a5f461e1bc34083baae3a4fd81
nathanielseth <nathanielseth.dev@gmail.com>
2023-09-19 17:14:25 +0800

    input fields

--- src/script.js
+++ src/script.js
@@ -8,19 +8,17 @@ const formatNumber = function (input) {
 let mybutton = document.getElementById('btn-back-to-top');
 
 const backToTop = function () {
+  mybutton.style.display = 'none';
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 };
 
-const scrollFunction = function () {
+mybutton.addEventListener('click', backToTop);
+
+window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = 'block';
   } else {
     mybutton.style.display = 'none';
   }
-};
-mybutton.addEventListener('click', backToTop);
-
-window.onscroll = function () {
-  scrollFunction();
 };
