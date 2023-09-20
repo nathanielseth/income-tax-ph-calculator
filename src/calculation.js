1776b21524fb821f01946eacbdaa883beefe165b
nathanielseth <nathanielseth.dev@gmail.com>
2023-09-20 12:58:33 +0800

    init tax calc function

---
+++ src/calculation.js
@@ -0,0 +1,155 @@
+'use strict';
+
+const philHealth = [
+  [NaN, 10000, () => 500],
+  [10000.01, 99999.99, (mon) => mon * 0.05],
+  [100000, NaN, () => 5000],
+];
+
+const bracket = (lower, upper) => (income) =>
+  (isNaN(lower) || income >= lower) && (isNaN(upper) || income <= upper);
+
+const bracketSSS = (lower, upper) => (income) =>
+  (isNaN(lower) || income >= lower) && (isNaN(upper) || income < upper);
+
+const computeSss = (salary) => {
+  const matrix = [
+    [1000, 3250, 135, 0],
+    [3250, 3750, 157.5, 0],
+    [3750, 4250, 180, 0],
+    [4250, 4750, 202.5, 0],
+    [4750, 5250, 225, 0],
+    [5250, 5750, 247.5, 0],
+    [5750, 6250, 270, 0],
+    [6250, 6750, 292.5, 0],
+    [6750, 7250, 315, 0],
+    [7250, 7750, 337.5, 0],
+    [7750, 8250, 360, 0],
+    [8250, 8750, 382.5, 0],
+    [8750, 9250, 405, 0],
+    [9250, 9750, 427.5, 0],
+    [9750, 10250, 450, 0],
+    [10250, 10750, 472.5, 0],
+    [10750, 11250, 495, 0],
+    [11250, 11750, 517.5, 0],
+    [11750, 12250, 540, 0],
+    [12250, 12750, 562.5, 0],
+    [12750, 13250, 585, 0],
+    [13250, 13750, 607.5, 0],
+    [13750, 14250, 630, 0],
+    [14250, 14750, 652.5, 0],
+    [14750, 15250, 675, 0],
+    [15250, 15750, 697.5, 0],
+    [15750, 16250, 720, 0],
+    [16250, 16750, 742.5, 0],
+    [16750, 17250, 765, 0],
+    [17250, 17750, 787.5, 0],
+    [17750, 18250, 810, 0],
+    [18250, 18750, 832.5, 0],
+    [18750, 19250, 855, 0],
+    [19250, 19750, 877.5, 0],
+    [19750, 20250, 900, 0],
+    [20250, 20750, 900, 22.5],
+    [20750, 21250, 900, 45],
+    [21250, 21750, 900, 67.5],
+    [21750, 22250, 990, 90],
+    [22250, 22750, 900, 112.5],
+    [22750, 23250, 900, 135],
+    [23250, 23750, 900, 157.5],
+    [23750, 24250, 900, 180],
+    [24250, 24750, 900, 202.5],
+    [24750, NaN, 900, 225],
+  ];
+
+  const sss = matrix
+    .filter((q) => bracketSSS(q[0], q[1])(salary))
+    .reduce((p, q) => (p += q[2]), 0);
+  const mpf = matrix
+    .filter((q) => bracketSSS(q[0], q[1])(salary))
+    .reduce((p, q) => (p += q[3]), 0);
+  return { sss, mpf };
+};
+
+const computePhilHealth = (monthly) => {
+  return (
+    philHealth
+      .filter((q) => bracket(q[0], q[1])(monthly))
+      .reduce((p, q) => (p += q[2](monthly)), 0) * 0.5
+  );
+};
+
+const brackets = [
+  {
+    condition: bracket(NaN, 250000),
+    calculate: (_) => 0,
+  },
+  {
+    condition: bracket(250000, 400000),
+    calculate: (q) => 0 + (q - 250000) * 0.15,
+  },
+  {
+    condition: bracket(400000, 800000),
+    calculate: (q) => 22500 + (q - 400000) * 0.2,
+  },
+  {
+    condition: bracket(800000, 2000000),
+    calculate: (q) => 102500 + (q - 800000) * 0.25,
+  },
+  {
+    condition: bracket(2000000, 8000000),
+    calculate: (q) => 402500 + (q - 2000000) * 0.3,
+  },
+  {
+    condition: bracket(8000000, NaN),
+    calculate: (q) => 2202500 + (q - 8000000) * 0.35,
+  },
+];
+
+const computeTaxDue = (taxableIncome) =>
+  brackets
+    .filter((q) => q.condition(taxableIncome))
+    .reduce((total, bracket) => (total += bracket.calculate(taxableIncome)), 0);
+
+function calculateTax() {
+  let monthlySalary = document.getElementById('monthlySalary').value;
+  let allowance = document.getElementById('allowance').value;
+  let allowanceTaxable = document.getElementById('allowanceTaxable').checked;
+  let sector = document.querySelector('input[name="sector"]:checked').value;
+  let period = document.querySelector('input[name="period"]:checked').value;
+
+  let sssContribution =
+    sector === 'private' ? computeSss(monthlySalary).sss : NaN;
+  let mpfContribution =
+    sector === 'private' ? computeSss(monthlySalary).mpf : NaN;
+  let gsisContribution = sector === 'private' ? monthlySalary * 0.09 : NaN;
+  let pagibigContribution = 100;
+  let philhealthContribution = computePhilHealth(monthlySalary);
+  let grossIncome = monthlySalary;
+
+  let totalDeductions =
+    sssContribution +
+    mpfContribution +
+    philhealthContribution +
+    pagibigContribution;
+
+  let taxableIncome = grossIncome - totalDeductions;
+  let taxDue = computeTaxDue(taxableIncome, period);
+
+  let factor = period === 'annually' ? 12 : period === 'bi-weekly' ? 26 : 1;
+  grossIncome *= factor;
+  sssContribution *= factor;
+  mpfContribution *= factor;
+  philhealthContribution *= factor;
+  pagibigContribution *= factor;
+  taxableIncome *= factor;
+  taxDue *= factor;
+
+  document.getElementById('grossIncome').innerText = grossIncome.toFixed(2);
+  document.getElementById('sssAmount').innerText = sssContribution.toFixed(2);
+  document.getElementById('mpfAmount').innerText = mpfContribution.toFixed(2);
+  document.getElementById('philhealthAmount').innerText =
+    philhealthContribution.toFixed(2);
+  document.getElementById('pagibigAmount').innerText =
+    pagibigContribution.toFixed(2);
+  document.getElementById('taxableIncome').innerText = taxableIncome.toFixed(2);
+}
--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 // format number for readability
-const formatNumber = function (input) {
-  let number = input.replace(/[^0-9]/g, '');
-  number = number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  return number;
-};
+// const formatNumber = function (input) {
+//   let number = input.replace(/[^0-9]/g, '');
+//   number = number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+//   return number;
+// };
 
 let mybutton = document.getElementById('btn-back-to-top');
 
@@ -22,3 +22,54 @@ window.onscroll = function () {
     mybutton.style.display = 'none';
   }
 };
+
+// type animation
+
+const textArray = [
+  'Calculator for PH income tax!',
+  'Calculate your MP2 investments!',
+  'Make it make sense!',
+  'Financial calculator for the netizens!',
+  'An open-source project!',
+  'Updated for 2024!',
+];
+
+const typewriterText = document.getElementById('typewriter-text');
+let textIndex = 0;
+let charIndex = 0;
+let isTyping = true;
+
+const type = function () {
+  if (charIndex < textArray[textIndex].length) {
+    typewriterText.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 60);
+  } else {
+    isTyping = false;
+    setTimeout(erase, 5500);
+  }
+};
+
+const erase = function () {
+  if (isTyping) {
+    typewriterText.textContent =
+      textArray[textIndex].substring(0, charIndex - 1) + '|';
+    isTyping = false;
+    setTimeout(erase, 25);
+  } else {
+    typewriterText.textContent = textArray[textIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    if (charIndex === 0) {
+      textIndex = (textIndex + 1) % textArray.length;
+      isTyping = true;
+      setTimeout(type, 1000);
+    } else {
+      setTimeout(erase, 50);
+    }
+  }
+};
+
+type();
